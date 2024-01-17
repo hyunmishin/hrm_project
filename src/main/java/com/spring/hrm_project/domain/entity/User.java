919742882131547ac52da629e6d09fc8a2b18ef8
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2024-01-17 15:57:39 +0900

    [fix] 로그인 에러 수정

--- src/main/java/com/spring/hrm_project/domain/entity/User.java
+++ src/main/java/com/spring/hrm_project/domain/entity/User.java
@@ -1,9 +1,6 @@
 package com.spring.hrm_project.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,12 +15,15 @@ public class User {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "login_id")
     private String loginId;
     private String password;
     private String nickname;
 
+    @Column(name = "user_role")
     private String userRole;
 
+    @Column(name = "access_token")
     private String accessToken;
 
     // OAuth 로그인에 사용
